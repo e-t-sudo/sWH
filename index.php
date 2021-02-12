3ae21f0f7104007d437c3c0397fd2d84c3e98113
e_t <77915530+e-t-sudo@users.noreply.github.com>
2021-02-12 16:54:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "includes/head.php"; ?>
+    <title>sWH</title>
+</head>
+<body>
+        <?php include "includes/header.php"; ?>
+        <br><br><br><br><br><h1 style="text-align: center;">Hello User</h1><br><br><br><br><br>
+        <?php include "includes/footer.php" ?>
+</body>
+</html>
