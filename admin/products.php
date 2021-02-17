9deb881894e21d331706e8d3eb95c14e7a3c60f5
e_t <e-t-sudo@users.noreply.github.com>
2021-02-17 18:18:19 +0530

    Update products.php

---
+++ admin/products.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "includes/head.php"; ?>
+    <title>Products</title>
+</head>
+<body>
+   <?php include "includes/header.php"; ?>
+   <br><br><br><br><br><br>
+	<h2 class="text-center">Products</h2>
+	<br><br><br><br><br><br>
+    <table class="table table-bordered table-striped text-center">
+	<thead>
+		<th></th><th>Product Name</th><th>List Price</th><th>Discount</th><th>Price</th><th>Rating</th>
+	</thead>
+	<tbody>
+		<?php
+		$conn = mysqli_connect("localhost", "root", "", "newbase");
+		$prod_query = "select * from products";
+		$result = mysqli_query($conn, $prod_query);
+		if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_array($result)){
+                $prod_id = $row['prod_id'];
+                $prod_title = $row['prod_title'];
+                $list_price = $row['list_price'];
+                $discount = $row['discount'];
+                $price = $list_price - ($list_price*$discount/100);
+                $prod_category = $row['prod_category'];
+                $description = $row['description'];
+                $rating = $row['rating']; ?>
+            <tr><td></td><td><?= $prod_title?></td><td><?= $list_price?></td><td><?=$discount ?></td><td><?=$price ?></td><td><?= $rating ?><i class="fa fa-star"></i></td></tr>
+            <?php }
+            } ?>
+	</tbody>
+	</table>
+</body>
+</html>
