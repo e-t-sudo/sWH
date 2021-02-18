0685042af9c608a090ea156a55873b7effddea0b
e_t <e-t-sudo@users.noreply.github.com>
2021-02-18 15:44:16 +0530

    Tag segregation interface

--- index.php
+++ index.php
@@ -7,10 +7,18 @@
         .card-img-top{
             height: 100px;
             width: 100px;
-            border-top-left-radius: 30px;
-            border-bottom-right-radius: 30px;
+        /*    border-top-left-radius: 30px;
+            border-bottom-right-radius: 30px;*/
+            border-radius: 5%;
             padding: 0px;
             margin: 0px;
+             box-shadow: 1px 1px 3px 1px grey;
+        }
+        .prod-title{
+            padding: 5px;
+            text-shadow: 1px 1px 3px 1px grey;
+            font-size: 20px;
+            color: #666666;
         }
         table{
             height: 130px; 
@@ -33,11 +41,54 @@
             text-decoration: none;
             color: black;          
         }
+        .tag-boxes{
+            padding: 10px;
+        }
+        .filter-btn{
+            margin: 20px;
+        }
     </style>
 </head>
 <body>
         <?php include "includes/header.php"; ?>
-        <br><br><br><br><br><hr><h1 style="text-align: center;">Featured Products</h1><hr>
+
+        <br><br><br>
+        <?php
+            function isPresent($ar, $val){
+                for($i=0; $i<sizeof($ar); $i++){
+                    if($ar[$i]==$val) return true;
+                }
+            return false;
+            }
+        ?>
+        <div>
+            <form class="form padding" method = "post" action="index.php">
+                <?php 
+                    $conn = mysqli_connect("localhost", "root", "", "newbase");
+                    $tag_query = "select * from tags";
+                    $result = mysqli_query($conn, $tag_query);
+                    if(mysqli_num_rows($result)>0){
+                        while($row = mysqli_fetch_assoc($result)){
+                            $tag_name = $row['tag'];
+                            $tag_id = $row['tag_id'];?>
+                    <div class="form-check form-check-inline tag-boxes">
+                        <input <?php if(!isset($_POST['tags'])||isPresent($_POST['tags'], $tag_id)) echo "checked"; ?> type = "checkbox" class="form-check-input" name="tags[]" value=<?= $tag_id?>><?= $tag_name?></input>
+                    </div>
+                <?php }
+                    }
+
+                ?>
+                <br>
+                <input type = "submit" class="btn btn-secondary btn-sm filter-btn" name="filter" value="Filter"></input>
+            </form>
+        </div>
+        <?php 
+            if(isset($_POST['tags'])){
+                $tags = $_POST['tags'];
+              // for($i=0; $i<sizeof($tags); $i++) echo $tags[$i];
+            }
+        ?>
+<br><br><hr><h1 style="text-align: center;">Featured Products</h1><hr>
         <div class="container-fluid text-center">
             <div class="row">
         <?php 
@@ -61,7 +112,7 @@
                             <td>
                                 <img class="card-img-top" src="./admin/uploads/product_images/<?= $prod_id?>.png" alt="Product">   
                                 <br>                            
-                                <h6><?= $prod_title?></h6>
+                                <h6 class="prod-title"><?= $prod_title?></h6>
                             </td>
                             <td class="prod-det">
                                         <span class="list-price text-danger">List Price: <s>$<?= $list_price ?></s></span><br>
@@ -85,6 +136,11 @@
         <hr>
         <br><br><br><br><br>
         <?php include "includes/footer.php" ?>
+        <script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+    </script>
 </body>
 </html>
 
