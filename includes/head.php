e99161049c4bbbdd48e1499d23e0e0b4899ecf8e
e_t <e-t-sudo@users.noreply.github.com>
2021-02-22 15:43:52 +0530

    Adding proper styles for the navigation links

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,4 @@
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="The e-commerse site for those who want everything delivered to them">
@@ -12,3 +12,80 @@
     <script src="https://use.fontawesome.com/d8fd830dde.js"></script>
 
     <link rel="stylesheet" href="includes/main.css">
+    <style>
+        .card-img-top{
+            height: 100px;
+            width: 100px;
+        /*    border-top-left-radius: 30px;
+            border-bottom-right-radius: 30px;*/
+            border-radius: 5%;
+            padding: 0px;
+            margin: 0px;
+             box-shadow: 1px 1px 3px 1px grey;
+        }
+        .prod-title{
+            padding: 5px;
+            text-shadow: 1px 1px 3px 1px grey;
+            font-size: 20px;
+            color: #666666;
+        }
+        table{
+            height: 130px; 
+            width: 80%;
+            padding-left: 0px;
+        }
+        .details-btn{
+            width: 100%;
+        }
+        .prod-det{
+            padding-left: 20px;
+        }
+        .fa-star{
+            color: #FF9529;
+        }
+        .tile{
+            color: black;
+        }
+        .tile:hover{
+            text-decoration: none;
+            color: black;          
+        }
+        .tag-boxes{
+            padding: 10px;
+        }
+        .filter-btn{
+            margin: 20px;
+        }
+        @media only screen and (max-width: 768px) {
+             #product-menu {
+        display: none;
+         }
+        }
+        
+        @media only screen and (min-width: 768px){
+            #smaller-product-menu{
+                display: none;
+            }
+        }
+        .no-style-links{
+            text-decoration: none;
+            color: #333333;
+        } .no-style-links:hover{
+            text-decoration: none;
+            color: #000000;
+            font-size: 1.01em;
+        }
+        @media only screen and (min-width: 768px){
+            .current-page{
+                border-bottom: solid 1px #ffffff;
+                font-size: 1.1em;
+            }
+        }
+        @media only screen and (max-width: 786px){
+            .current-page{
+                font-size: 1.2rem;
+                color: #090909!important;
+            }
+                
+        }
+    </style>
