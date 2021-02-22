8ebb8e09632245dc1b68d64cb0100eff98f21f72
e_t <e-t-sudo@users.noreply.github.com>
2021-02-22 15:45:33 +0530

    Dynamic navigation bar

--- includes/header.php
+++ includes/header.php
@@ -14,13 +14,13 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php">Home</a>
+                            <a class="nav-link <?php if($page=="home") echo "current-page"; ?>" href="index.php">Home</a>
                         </li>
                         <li class="nav-item dropdown" id="product-menu">
-                            <a class="nav-link dropdown-toggle" href="notAvailable.php" data-toggle="dropdown">Products<span class="caret"></span></a>
-                            <div class="dropdown-menu mega-menu" aria-labelledby="dropdownMenuButton" style="background-color: white!important;width:calc(50vw); position: absolute; left: -300px;  box-shadow: 2px 2px #f3f3f3;  ">
-                                                <div class="container-fluid" style="background-color: white!important; width: 100%; ">
-                                                    <div class="row text-center" style="background-color: white!important">
+                            <a class="nav-link dropdown-toggle <?php if($page=="products") echo "current-page"; ?>" href="notAvailable.php" data-toggle="dropdown">Products<span class="caret"></span></a>
+                            <div class="dropdown-menu mega-menu" aria-labelledby="dropdownMenuButton" style="background-color: #f6f6f6!important;width:calc(50vw); position: absolute; left: -300px;  box-shadow: 2px 2px #f3f3f3;  ">
+                                                <div class="container-fluid" style="background-color: #f6f6f6!important; width: 100%; ">
+                                                    <div class="row text-center" style="background-color: #f6f6f6!important">
                                                 <?php
                                                         $conn = mysqli_connect("localhost", "root", "", "newbase");
                                                         $sql_query = "select * from categories where `parent` = 0 and `display` = 1";
@@ -31,13 +31,14 @@
                                                                 $cat_id = $row['cat_id'];
                                                 ?>
                                                                 <div class="col-md-6 menu-items text-left">
-                                                                    <h4><?= $category ?></h4> 
+                                                                <a class="no-style-links" href="products.php?category=<?= $cat_id?>"><h4><?= $category ?></h4></a>
                                                                     <div class="mega-menu-list">
                                                                         <?php $sub_query = "select * from categories where `parent` = $cat_id and `display` = 1";
                                                                                 $sub_result = mysqli_query($conn, $sub_query);
                                                                                 while($row = mysqli_fetch_assoc($sub_result)){
+                                                                                    $cat_id = $row['cat_id'];
                                                                                     $sub_category = $row['category']; ?>
-                                                                                    <p class="mega-menu-list-item" style="padding-left: 20px;padding-right: 30px; "><?= $sub_category ?></p>
+                                                                                    <p class="mega-menu-list-item" style="padding-left: 20px;padding-right: 30px; "><a class="no-style-links" href="products.php?category=<?= $cat_id?>"><?= $sub_category ?></a></p>
                                                                 <?php        } ?>
                                                                     </div>
                                                                 </div>
@@ -56,24 +57,27 @@
                             if(mysqli_num_rows($query_result)>0){
                                 while($row = mysqli_fetch_assoc($query_result)){
                                     $category = $row['category'];
-                                    $cat_id = $row['cat_id'];
-                                    print('<li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="notAvailable.php" data-toggle="dropdown">'.$category.'<span class="caret"></span></a>
-                                    <ul class="dropdown-menu" role="menu">');
+                                    $cat_id = $row['cat_id']; ?>
+                                    <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="products.php?category=<?= $cat_id?>" data-toggle="dropdown"><?= $category?><span class="caret"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                    <?php
                                         $sql_query = "select * from categories where `parent` = $cat_id and `display` = 1";
                                         $sub_query_result = mysqli_query($conn, $sql_query);
                                         if(mysqli_num_rows($sub_query_result)>0){
                                             while($row = mysqli_fetch_assoc($sub_query_result)){
                                                 $sub_category = $row['category'];
-                                                print('<li><a class="dropdown-item" href="notAvailable.php">'.$sub_category.'</a></li>');
+                                                $cat_id = $row['cat_id']; ?>
+                                                    <li><a class="dropdown-item" href="products.php?category=<?= $cat_id ?>"><?= $sub_category?></a></li>
+                                            <?php
                                             }
                                         }else{
                                             echo "No results for sub-categories";
-                                        }
-                                    print('</ul>
-                                </li>');
+                                        } ?>
+                                    </ul>
+                                </li>
 
-                                }
+                          <?php      }
                             }else{
                                 echo "No results for categories";
                             }
@@ -81,10 +85,10 @@
                         </div>
                         
                         <li class="nav-item">
-                            <a class="nav-link" href="notAvailable.php">About</a>
+                            <a class="nav-link <?php if($page=="about") echo "current-page"; ?>" href="about.php">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="notAvailable.php">Contacts</a>
+                            <a class="nav-link <?php if($page=="contact") echo "current-page"; ?>" href="contact.php">Contacts</a>
                         </li>
                     </ul>
                 </div>
